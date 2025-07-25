a20e2ac4b9c06a3783ce13c14c68203864be7e65
liddd25 <lidyaa.mes21@gmail.com>
2025-07-25 08:58:47 +0300

    Create decrypt_string.py

---
+++ decrypt_string.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def freqAlphabets(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = []
+        i = len(s) - 1
+        
+        while i >= 0:
+            if s[i] == '#':
+                num = int(s[i-2:i])
+                res.append(chr(ord('a') + num - 1))
+                i -= 3
+            else:
+                num = int(s[i])
+                res.append(chr(ord('a') + num - 1))
+                i -= 1
+        
+        return ''.join(res[::-1])
